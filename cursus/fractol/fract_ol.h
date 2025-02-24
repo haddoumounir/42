df5d99beb61d74da4e94d91edc35435006b6fc59
mounir haddou <mounirhaddou.mh@gmail.com>
2025-02-24 09:03:43 +0000

    Update fractol - day 2

--- cursus/fractol/fract_ol.h
+++ cursus/fractol/fract_ol.h
@@ -6,7 +6,7 @@
 /*   By: mhaddou <mhaddou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 08:36:50 by mhaddou           #+#    #+#             */
-/*   Updated: 2025/02/23 15:07:47 by mhaddou          ###   ########.fr       */
+/*   Updated: 2025/02/23 18:08:19 by mhaddou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,32 @@
 
 #define WIDTH 800
 #define HEIGHT 800
-#define MAX_TH_THERATION 50
-#define MAX_VALID 2
-#define MIN_VALID -2
+#define MAX_TH_THERATION 42
+#define MAX_VALID 799
+#define MIN_VALID 0
 #define MAX 2
-#define MIN 2
+#define MIN -2
+#define ESCAPE_VALUER 1
+
+
+
+
+#define BLACK  0x000000
+#define WHITE  0xFFFFFF
+#define RED    0xFF0000
+#define GREEN  0x00FF00
+#define BLUE   0x0000FF
+#define YELLOW 0xFFFF00
+#define CYAN   0x00FFFF
+#define MAGENTA 0xFF00FF
+#define ORANGE 0xFFA500
+#define PURPLE 0x800080
+#define PINK   0xFFC0CB
+#define BROWN  0xA52A2A
+#define GRAY   0x808080
+#define LIGHT_GRAY 0xD3D3D3
+#define DARK_GRAY  0xA9A9A9
+
 
 size_t	ft_strlen(const char *s);
 void	ft_putstr_fd(char *s, int fd);
--- cursus/fractol/ft_mandelbrot.c
+++ cursus/fractol/ft_mandelbrot.c
@@ -6,51 +6,52 @@
 /*   By: mhaddou <mhaddou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 09:46:29 by mhaddou           #+#    #+#             */
-/*   Updated: 2025/02/23 15:10:51 by mhaddou          ###   ########.fr       */
+/*   Updated: 2025/02/24 08:58:15 by mhaddou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fract_ol.h"
-int sacn(complex val)
+
+int sacn(void)
 {
-    complex val;
-    val.r = 0;
-    val.i = 0;
+    complex z;
+    z.r = 0;
+    z.i = 0;
     complex c;
-    
     c.r = 0.25;
     c.i = 0.4;
-
     printf("\ndebug :%p\n",&c);
-    //sacn()
     int i = 0;
     while (i < MAX_TH_THERATION)
     {
-        double r = (val.r*val.r) - (val.i * val.i);
-        val.i = 2 * (val.i) * (val.r);
-        val.r = r ;
-        val.r+=c.r;
-        val.i+=c.i;
-        printf("%d---->%f %f \n",i,val.i,val.r);
-        i++;
+        double r = pow(z.i,2) - pow(z.r,2);
+        z.i = 2 * (z.i) * (z.r);
+        z.r = r ;
+        z.r+=c.r;
+        z.i+=c.i;
+        if(pow(z.i,2)+pow(z.r,2) > ESCAPE_VALUER)
+        {
+            return(0);
+        }
+        printf("%d---->%f %f \n",i,z.i,z.r);
+        i++; 
     }
-    
     return(0);
 }
-double scaleBetween(double number)
+//btween [0,799] --> [-2,+2] 
+double scaleBetween(double number, double minAllowed, double maxAllowed, double min, double max) 
 {
-    number = (number - MIN ) / MAX-MIN;
-    number = number * (MAX_VALID-MIN_VALID);
-    
+    number -= min;
+    number /=(max - min);
+    return (maxAllowed - minAllowed) * number + minAllowed;
 }
+
 int ft_mandelbrot(void)
 {
-    
-    //pre_mlx("mandelbrot");
     int i = 0;
-    while (i)
+    while (i < 800)
     {
-        printf("%f",scaleBetween(i));
+        printf("%d --> %f\n",i,scaleBetween((double)i,-2,2,0,799));
         i++;
     }
     
